19098b94ed81a8718056728b24ba642a3371ec2e
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-05-03 23:22:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
+  document.getElementById("sampleTable").innerHTML=`<tr><td>New Cell1</td> 
+<td>New Cell2</td> </tr>
+<tr><td>Row1 cell1</td> 
+<td>Row1 cell2</td></tr> 
+<tr><td>Row2 cell1</td> 
+<td>Row2 cell2</td></tr> `
   
 }
