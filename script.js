ba51f709033d4219848da97ae98d8dd45d065fcb
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-05-03 23:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function insert_Row() {
     //Write your code here
-  document.getElementById("sampleTable").innerHTML=`<tr><td>New Cell1</td> 
-<td>New Cell2</td> </tr>
-<tr><td>Row1 cell1</td> 
-<td>Row1 cell2</td></tr> 
-<tr><td>Row2 cell1</td> 
-<td>Row2 cell2</td></tr> `
-  
+//   document.getElementById("sampleTable").innerHTML=`<tr><td>New Cell1</td> 
+// <td>New Cell2</td> </tr>
+// <tr><td>Row1 cell1</td> 
+// <td>Row1 cell2</td></tr> 
+// <tr><td>Row2 cell1</td> 
+// <td>Row2 cell2</td></tr> `
+
+let table=document.getElementById("sampleTable")
+let row = table.insertRow(0);
+let td1=row.insertCell(0);
+let td2=row.insertCell(1);
+
+td1.innerHTML="New Cell1"
+td2.innerHTML="New Cell2"
 }
